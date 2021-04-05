ab1cabd287e2e6300ae7ca313f6dd5a2b9c72f8a
Nija <nija.perry21@gmail.com>
2021-04-05 09:48:14 -0400

    commit tree b55d6e06255f113efc5cd39469850a7ecb2d8008

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 /* Your Code Here */
+const createEmployeeRecord = (str1, str2, str3, num) => {
+
+    firstName: str1
+    familyName: str2
+    title: str3
+    payPerHour: num
+
+
+}
+
+const createEmployeeRecords = () => {
+
+}
+
+const createTimeInEvents = () => {
+
+}
+
+const createTimeInEvent = () => {
+
+}
+
+const createTimeOutEvent = () => {
+
+}
+
+const hoursWorkedOnDate = () => {
+
+}
+
+const wagesEarnedOnDate = () => {
+
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +54,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+
+const findEmployeeByFirstName = () => {
+
+}
+
+const calculatePayroll = () => {
+
 }
